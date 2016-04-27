27c6e9851987c98a8b9d825c75a9ef2551e4b432
Zeke Sikelianos <zeke@sikelianos.com>
2016-04-27 16:02:56 -0700

    remove jquery dependency

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-$ = jquery
-
 var updateDownloadLink = function () {
   var platform = uaParserJs().os
   var releaseServer = 'https://electron-api-demos.githubapp.com/updates/'
@@ -14,12 +12,11 @@ var updateDownloadLink = function () {
   // Don't be clever if platform is something other than windows or mac
   if (!platform.id) return
 
-  $('#download-latest-release')
-    .attr('href', releaseServer + filenames[platform.id])
-    .find('.label')
-    .text('Download for ' + platform.name)
+  var button = document.getElementById('download-latest-release')
+  button.setAttribute('href', releaseServer + filenames[platform.id])
+  button.textContent = 'Download for ' + platform.name
 
-  $('#download-alternatives').show()
+  document.getElementById('download-alternatives').style.display = 'inline-block'
 }
 
-$(updateDownloadLink)
+document.addEventListener('DOMContentLoaded', updateDownloadLink)
