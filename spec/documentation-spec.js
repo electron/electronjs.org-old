fde55ec9de0b8766e0572abb83cd0ebe0aa461fc
Kevin Sawicki <kevinsawicki@gmail.com>
2016-05-18 16:39:42 -0700

    Remove v from version

--- spec/documentation-spec.js
+++ spec/documentation-spec.js
@@ -26,7 +26,7 @@ test('Fetch and write documentation', function (t) {
 
     var config = yaml.load(settings.configFile)
 
-    t.equal(config.latest_version, settings.version, 'Config: Latest version set.')
+    t.equal(config.latest_version, settings.version.substring(1), 'Config: Latest version set.')
 
     var sampleDoc = fs.readFileSync(path.join(settings.targetDir, 'api', 'accelerator.md'))
     var sampleFM = frontmatter.loadFront(sampleDoc)
