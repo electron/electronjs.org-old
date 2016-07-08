4cc0d4738b9e4f91a41826e3a18ff3f5bfa60fb6
Kevin Sawicki <kevinsawicki@gmail.com>
2016-07-08 12:21:15 -0700

    Assert each API page has a process

---
+++ spec/processes-spec.js
@@ -0,0 +1,25 @@
+var fs = require('fs')
+var frontmatter = require('yaml-front-matter')
+var path = require('path')
+var test = require('tape')
+var yaml = require('yamljs')
+
+test('Include process types for each API documentation page', function (t) {
+  var docsPath = path.join(__dirname, '..', '_docs', 'api')
+  var processes = yaml.load(path.join(__dirname, '..', '_data', 'processes.yml'))
+
+  fs.readdirSync(docsPath).filter(function (file) {
+    return path.extname(file) == '.md'
+  }).map(function (file) {
+    return path.join(docsPath, file)
+  }).forEach(function (markdownFile) {
+    var apiDoc = fs.readFileSync(markdownFile)
+    var config = frontmatter.loadFront(apiDoc)
+    var apiProcess = processes[config.title]
+    t.ok(apiProcess === 'Main Process'
+      || apiProcess === 'Renderer Process'
+      || apiProcess === 'Main and Renderer Process',
+      config.title + ' has process value of ' + apiProcess)
+  })
+  t.end()
+})
