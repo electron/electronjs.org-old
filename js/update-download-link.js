e48041158dac853642d8bc2bd4379c656d3c1569
Zeke Sikelianos <zeke@sikelianos.com>
2016-09-26 10:33:05 -0700

    run syntax-fixing scripts everywhere, not just on docs pages

---
+++ js/update-download-link.js
@@ -0,0 +1,30 @@
+var updateDownloadLink = function () {
+  var platform = uaParserJs().os.name
+  var releaseServer = 'https://electron-api-demos.githubapp.com/updates/'
+  var assetName
+  var osLabel
+
+  if (/mac/i.test(platform)) {
+    assetName = 'electron-api-demos-mac.zip'
+    osLabel = 'Mac'
+  } else if (/windows/i.test(platform)) {
+    assetName = 'ElectronAPIDemosSetup.exe'
+    osLabel = 'Windows'
+  } else if (/ubuntu|linux/i.test(platform)) {
+    assetName = 'electron-api-demos-linux.zip'
+    osLabel = 'Linux'
+  } else {
+    return
+  }
+
+  document.querySelector('#download-latest-release')
+    .setAttribute('href', releaseServer + assetName)
+
+  document.querySelector('#download-latest-release .label')
+    .textContent = 'Download for ' + osLabel
+
+  document.querySelector('#download-alternatives')
+    .style.display = 'inline-block'
+}
+
+document.addEventListener('DOMContentLoaded', updateDownloadLink)
