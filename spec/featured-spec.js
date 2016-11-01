cf733a370b0a34ccb092374246ecb1a318510e33
Zeke Sikelianos <zeke@sikelianos.com>
2016-11-01 10:29:53 -0700

    feature four more apps on homepage

--- spec/featured-spec.js
+++ spec/featured-spec.js
@@ -2,12 +2,12 @@ var path = require('path')
 var test = require('tape')
 var yaml = require('yamljs')
 
-test('Limit featured homepage apps to 20', function (t) {
+test('Limit featured homepage apps to 24', function (t) {
   t.plan(1)
 
   var apps = yaml.load(path.join(__dirname, '..', '_data', 'apps.yml'))
   var featuredApps = apps.filter(function (app) {
     return app.featuredOnHomepage
   })
-  t.equal(featuredApps.length, 20, '20 apps should be featured instead of ' + featuredApps.length)
+  t.equal(featuredApps.length, 24, '24 apps should be featured instead of ' + featuredApps.length)
 })
