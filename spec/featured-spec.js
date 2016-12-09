954f25ce4b5a488db7bf3119043cba8eb06fb2a2
Zeke Sikelianos <zeke@sikelianos.com>
2016-12-09 14:00:34 -0800

    use windows-friendly path joining

--- spec/featured-spec.js
+++ spec/featured-spec.js
@@ -4,6 +4,6 @@ var yaml = require('yamljs')
 
 test('Limit featured homepage apps to 24', function (t) {
   t.plan(1)
-  var featuredApps = require(path.join(__dirname, '../_data/featured_apps.json'))
+  var featuredApps = require(path.join(__dirname, '..', '_data', 'featured_apps.json'))
   t.equal(featuredApps.length, 24, '24 apps should be featured instead of ' + featuredApps.length)
 })
