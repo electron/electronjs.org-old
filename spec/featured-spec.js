2c6ea42bf265d46718d3f4ece4891745817909a5
Zeke Sikelianos <zeke@sikelianos.com>
2016-12-09 11:43:49 -0800

    move featuredOnHomePage property out of app metadata

--- spec/featured-spec.js
+++ spec/featured-spec.js
@@ -4,10 +4,6 @@ var yaml = require('yamljs')
 
 test('Limit featured homepage apps to 24', function (t) {
   t.plan(1)
-
-  var apps = yaml.load(path.join(__dirname, '..', '_data', 'apps.yml'))
-  var featuredApps = apps.filter(function (app) {
-    return app.featuredOnHomepage
-  })
+  var featuredApps = require(path.join(__dirname, '../_data/featured_apps.json'))
   t.equal(featuredApps.length, 24, '24 apps should be featured instead of ' + featuredApps.length)
 })
