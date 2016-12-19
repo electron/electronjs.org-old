2a87704d7fd2086f1a4aeb7303fab75ed716bc9b
Zeke Sikelianos <zeke@sikelianos.com>
2016-12-19 10:26:41 -0800

    remove unused test files

--- spec/featured-spec.js
+++
@@ -1,9 +0,0 @@
-var path = require('path')
-var test = require('tape')
-var yaml = require('yamljs')
-
-test('Limit featured homepage apps to 24', function (t) {
-  t.plan(1)
-  var featuredApps = require(path.join(__dirname, '..', '_data', 'featured_apps.json'))
-  t.equal(featuredApps.length, 24, '24 apps should be featured instead of ' + featuredApps.length)
-})
