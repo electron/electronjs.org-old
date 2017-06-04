94dcd16161ebebe50a9ea296ace0e67067838b63
Zeke Sikelianos <zeke@sikelianos.com>
2017-06-03 22:51:27 -0700

    it freaking works

--- server.js
+++ server.js
@@ -3,22 +3,39 @@ const path = require('path')
 const express = require('express')
 const sass = require('node-sass-middleware')
 const port = Number(process.env.PORT) || 5000
+const jexodus = require('./lib/jexodus')(__dirname).on('ready', startServer)
 const app = express()
 
 app.use(sass({
-    src: __dirname,
-    dest: __dirname,
+    src: path.join(__dirname, '_sass'),
+    dest: path.join(__dirname, 'css'),
     debug: true,
     // outputStyle: 'compressed',
-    // prefix:  '/prefix'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+    prefix: '/css'
 }))
 
-app.use(express.static(__dirname))
-const jexodus = require('./lib/jexodus')(__dirname).on('ready', startServer)
+const middleware = function (req, res, next) {
+  const page = jexodus.routes[req.path]
+  console.log(req.path)
+  if (page) {
+    return jexodus.render(req.path).then(output => res.send(output))
+  } else {
+    return next()
+  }
+}
 
-app.get('/', function (req, res) {
-  jexodus.render('/').then(output => res.send(output))
-})
+app.use(middleware)
+
+app.use(express.static(__dirname))
+
+
+// app.get('/', function (req, res) {
+//   jexodus.render('/').then(output => res.send(output))
+// })
+
+// app.get('/docs', function (req, res) {
+//   jexodus.render('/docs').then(output => res.send(output))
+// })
 
 function startServer () {
   if (module.parent) return
