77e6f5644be2d485b48a219f44d2a24a5c6fd9e6
Shelley Vohr <shelley.vohr@gmail.com>
2017-10-15 20:28:32 -0700

    add development section

--- routes/docs/category.js
+++ routes/docs/category.js
@@ -1,10 +1,16 @@
 module.exports = (req, res) => {
   const category = req.params.category
-  if (['tutorial', 'api', 'advanced'].includes(category)) {
+  if (['tutorial', 'api', 'development'].includes(category)) {
+    const categories = {
+      'tutorial': 'Guides',
+      'api': 'API',
+      'development': 'Development',
+    }
+
     const capitalize = str => str.charAt(0).toUpperCase() + str.substring(1);
     const context = Object.assign(req.context, {
       layout: 'docs',
-      category: capitalize(category)
+      category: categories[category]
     })
 
     res.render(`docs/${category}`, context)
