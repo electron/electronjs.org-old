72529a2a5d22ab177553ca312a81b22f6d74ef44
Zeke Sikelianos <zeke@sikelianos.com>
2017-10-20 21:18:09 -0700

    lint

--- scripts/update-app-download-links.js
+++ scripts/update-app-download-links.js
@@ -1,19 +1,32 @@
 const {getPlatformFromUserAgent, getPlatformLabel} = require('platform-utils')
 
+// Emphasize app download links for the visitor's OS (darwin|linux|win32)
+// Platform is inferred from user agent
+
 module.exports = function updateAppDownloadLinks () {
-  const appDownloadLinks = Array.from(document.querySelectorAll('a.app-download'))
-  if (!appDownloadLinks.length) return
+  const links = Array.from(document.querySelectorAll('a.app-download'))
+
+  // No downloads links found on the page.
+  if (!links.length) return
 
   const platform = getPlatformFromUserAgent()
+  const matches = links.filter(link => link.classList.contains(platform))
 
-  const platformMatches = appDownloadLinks.filter(link => {
-    return link.classList.contains(platform)
+  // No downloads found for the detected OS. Leave links untouched.
+  if (!matches.length) return
+
+  links.forEach(link => {
+    link.style.display = link.classList.contains(platform)
+      ? 'inline-block'
+      : 'none'
   })
 
-  console.log(platformMatches)
-  if (platformMatches.length == 1) {
-    appDownloadLinks.forEach(link => link.style.display = 'none')
-    platformMatches[0].style.display = 'inline-block'
+  if (matches.length === 1) {
+    matches[0].textContent = `Download for ${getPlatformLabel(platform)}`
+  }
+
+  if (links.length > 1) {
     document.querySelector('#view-all-downloads').style.display = 'inline-block'
   }
+  
 }
