6309c95993dfa6f27d2ac31ffe8b779046957eef
Zeke Sikelianos <zeke@sikelianos.com>
2017-10-20 19:59:21 -0700

    lint

--- scripts/apply-active-class-to-active-links.js
+++ scripts/apply-active-class-to-active-links.js
@@ -1,6 +1,6 @@
 module.exports = function applyActiveClassToActiveLinks () {
-  const topPath = location.pathname.split("/")[1]
-  
+  const topPath = location.pathname.split('/')[1]
+
   // homepage
   if (!topPath.length) return
 
