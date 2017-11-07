a462225b576e0f3a7283f465ba570924a6d76cbc
Pierre Neter <pierreneter@gmail.com>
2017-11-07 14:42:52 +0700

    Catch Not Found pages of /docs and add title for categories of docs. (/docs/404)

--- routes/docs/category.js
+++ routes/docs/category.js
@@ -2,25 +2,47 @@ const processes = require('../../data/processes.json')
 const versions = require('../../data/versions.json')
 
 module.exports = (req, res) => {
+  const categoryList = [
+    { path: 'all', name: 'All the Electron Docs!' },
+    { path: 'tutorial', name: 'Guides' },
+    { path: 'api', name: 'API' },
+    { path: 'development', name: 'Development' },
+    { path: 'versions', name: 'Versions' }
+  ]
   const category = req.params.category
+  const selectedCategories = categoryList.filter(cat => cat.path === category)
   let context
-  if (['tutorial', 'api', 'development'].includes(category)) {
-    const categories = {
-      'tutorial': 'Guides',
-      'api': 'API',
-      'development': 'Development'
-    }
 
-    context = Object.assign(req.context, {
-      layout: 'docs',
-      processes: processes,
-      category: categories[category]
-    })
-  } else if (category === 'versions') {
-    context = Object.assign(req.context, {
-      layout: 'docs',
-      versions: versions
-    })
+  if (selectedCategories.length > 0) {
+    const selectedCategory =  selectedCategories[0]
+    if (category === 'all') {
+      context = Object.assign(req.context, {
+        page: {
+          title: `${selectedCategory.name} | Electron`
+        },
+        layout: 'docs',
+        viewingAllDocs: true
+      })
+    } else if (category === 'versions') {
+      context = Object.assign(req.context, {
+        page: {
+          title: `${selectedCategory.name} | Electron`
+        },
+        layout: 'docs',
+        versions: versions
+      })
+    } else {
+      context = Object.assign(req.context, {
+        page: {
+          title: `${selectedCategory.name} | Electron`
+        },
+        layout: 'docs',
+        processes: processes,
+        category: selectedCategory.name
+      })
+    }
+    res.render(`docs/${category}`, context)
+  }  else {
+    res.status(404).render('404')
   }
-  res.render(`docs/${category}`, context)
 }
