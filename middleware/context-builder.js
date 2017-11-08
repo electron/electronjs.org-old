1015d828faead4852d94b4a714f0d307e9e8955d
Zeke Sikelianos <zeke@sikelianos.com>
2017-11-08 12:48:16 -0800

    display current language in header nav

--- middleware/context-builder.js
+++ middleware/context-builder.js
@@ -1,6 +1,7 @@
 const i18n = require('../lib/i18n')
 const vendoredVersions = require('../data/versions.json')
   .find(version => version.version === i18n.electronLatestStableVersion)
+const {getLanguageNativeName} = require('locale-code')
 
 // Supply all route handlers with a baseline `req.context` object
 module.exports = function contextBuilder (req, res, next) {
@@ -10,6 +11,7 @@ module.exports = function contextBuilder (req, res, next) {
     electronLatestStableTag: i18n.electronLatestStableTag,
     vendoredVersions: vendoredVersions,
     currentLocale: req.language,
+    currentLocaleNativeName: getLanguageNativeName(req.language),
     locales: i18n.locales,
     page: i18n.website[req.language].pages[req.path],
     localized: i18n.website[req.language],
