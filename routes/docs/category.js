b51bcc90bd97ae77a8f557ba45062ced51d94819
Pierre Neter <pierreneter@gmail.com>
2017-11-09 17:31:44 +0700

    Update and add tests

--- routes/docs/category.js
+++ routes/docs/category.js
@@ -13,36 +13,25 @@ module.exports = (req, res, next) => {
   const selectedCategories = categoryList.filter(cat => cat.path === category)
   let context
 
-  if (selectedCategories.length > 0) {
-    const selectedCategory =  selectedCategories[0]
-    if (category === 'all') {
-      context = Object.assign(req.context, {
-        page: {
-          title: `${selectedCategory.name} | Electron`
-        },
-        layout: 'docs',
-        viewingAllDocs: true
-      })
-    } else if (category === 'versions') {
-      context = Object.assign(req.context, {
-        page: {
-          title: `${selectedCategory.name} | Electron`
-        },
-        layout: 'docs',
-        versions: versions
-      })
-    } else {
-      context = Object.assign(req.context, {
-        page: {
-          title: `${selectedCategory.name} | Electron`
-        },
-        layout: 'docs',
-        processes: processes,
-        category: selectedCategory.name
-      })
-    }
-    res.render(`docs/${category}`, context)
-  }  else {
-    next()
+  if (!selectedCategories.length) return next()
+
+  const selectedCategory = selectedCategories[0]
+  if (category === 'all') {
+    context = Object.assign(req.context, {
+      layout: 'docs',
+      viewingAllDocs: true
+    })
+  } else if (category === 'versions') {
+    context = Object.assign(req.context, {
+      layout: 'docs',
+      versions: versions
+    })
+  } else {
+    context = Object.assign(req.context, {
+      layout: 'docs',
+      processes: processes,
+      category: selectedCategory.name
+    })
   }
+  res.render(`docs/${category}`, context)
 }
