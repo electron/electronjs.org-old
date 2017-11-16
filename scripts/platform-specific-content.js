e990ab8d8d717bc1da552ae44f73bf72e8a628a3
Zeke Sikelianos <zeke@github.com>
2017-11-16 07:47:05 -0800

    new site blog post (#835)
    * new website blog post WIP
    
    * website post notes
    
    * add figure templates
    
    * incremental progress on blog post
    
    * watch markdown changes
    
    * a little more blog post progress
    
    * add chinese nav screenshot
    
    * write about homebrew cask
    
    * use post layout
    
    * give the logo breathing room on tablet
    
    * blog post updates
    
    * dat screenshot
    
    * improve blog styles
    
    * getting closer
    
    * remove doublespaces
    
    * call it GitHub Desktop
    
    * fix typos and link to node
    
    * alphabetize markdown URL list

--- scripts/platform-specific-content.js
+++ scripts/platform-specific-content.js
@@ -1,21 +1,23 @@
-// Reveal hidden elements specific to the user's OS
-//
+// Reveal hidden elements that are specific to the user's OS
+// 
 // To use: apply a class of `darwin-only`, `linux-only`, or `win32-only`
 // to any element. It will be hidden by default, and revealed on page load.
-//
-// Elements will be displayed as `block` by default. To use a different display
-// value, add a class with `display-` as a prefix, like `display-inline-block`
-// or `display-table-row`, e.g <tr class="darwin-only display-table-row">
+// 
+// Elements will be displayed as `block` by default. To use another display
+// value, add a class to the element like `display-inline-block` or
+// `display-table-row`
 
 const {getPlatformFromUserAgent} = require('platform-utils')
 
 module.exports = function platformSpecificContent () {
   const platform = getPlatformFromUserAgent()
+  const elements = Array.from(document.querySelectorAll(`.${platform}-only`))
 
-  Array.from(document.querySelectorAll(`.${platform}-only`))
-    .forEach(element => {
-      console.log(element)
-      const displayType = Array.from(element.classList).find(c => c.startsWith('display-'))
-      element.style.display = displayType ? displayType.replace('display-', '') : 'block'
-    })
+  elements.forEach(el => {
+    const classes = Array.from(el.classList)
+    console.log(classes)
+    let displayType = classes.find(c => c.startsWith('display-'))
+    displayType = displayType ? displayType.replace('display-', '') : 'block'
+    el.style.display = displayType
+  })
 }
