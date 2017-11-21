6d3a2c9768b22c9f5ba942ad18a7d3f8a13b11a9
Zeke Sikelianos <zeke@sikelianos.com>
2017-11-21 13:36:36 -0800

    use colored placeholders for icons and fade images in when loaded

--- scripts/lazy-load-images.js
+++ scripts/lazy-load-images.js
@@ -2,10 +2,19 @@ const inView = require('in-view')
 
 module.exports = function () {
   inView('img[data-src]')
-    .on('enter', el => {
-      el.setAttribute('src', el.dataset.src)
-    })
-    .on('exit', el => {
-      // nothin'
+    .on('enter', img => {
+      // abort if this element has previously entered the viewport
+      if (!img.dataset.src) return
+
+      img.setAttribute('src', img.dataset.src)
+
+      // remove attribute so CSS fade-in can happen
+      img.onload = () => {
+        img.removeAttribute('data-src')
+        
+        // remove colored circular placeholder element
+        const placeholder = img.parentElement.querySelector('.placeholder')
+        if (placeholder) placeholder.classList.add('faded-out')
+      }
     })
 }
