165091b5e9692417b4f48a6ce3b2759746464906
Pierre Neter <pierreneter@gmail.com>
2017-11-30 02:36:03 +0700

    move logic into one place, avoid the 404 case appears suddenly

--- middleware/context-builder.js
+++ middleware/context-builder.js
@@ -2,6 +2,7 @@ const i18n = require('../lib/i18n')
 const vendoredVersions = require('../data/versions.json')
   .find(version => version.version === i18n.electronLatestStableVersion)
 const {getLanguageNativeName} = require('locale-code')
+const _ = require('lodash')
 
 // Supply all route handlers with a baseline `req.context` object
 module.exports = function contextBuilder (req, res, next) {
@@ -10,7 +11,6 @@ module.exports = function contextBuilder (req, res, next) {
 
   // Page titles, descriptions, etc
   const page = i18n.website[req.language].pages[req.path] || {}
-  page.path = req.path
 
   req.context = {
     electronLatestStableVersion: i18n.electronLatestStableVersion,
@@ -23,11 +23,17 @@ module.exports = function contextBuilder (req, res, next) {
     localized: i18n.website[req.language],
     cookies: req.cookies
   }
-
-  if (req.path !== '/' && req.context.page && !req.context.page.titled) {
-    req.context.page.titled = true
-    req.context.page.title = `${req.context.page.title} | Electron`
+  
+  if (req.path === '/') {}
+  else if (!req.context.page.titled) {
+    if (!_.isEmpty(req.context.page)) {
+      req.context.page.title = `${req.context.page.title} | Electron`
+    } else {
+      req.context.page.title = '404 Not Found | Electron'
+    }
   }
+  req.context.page.titled = true
+  req.context.page.path = req.path
 
   if (req.path.startsWith('/docs')) {
     req.context.docs = i18n.docs[req.language]
--- routes/_404.js
+++ routes/_404.js
@@ -1,4 +1,3 @@
 module.exports = (req, res) => {
-  req.context.page.title = '404 Not Found | Electron'
   res.render('404', req.context)
 }
