34451e3c82a179ce83a64e1e04f835e87f403493
Pierre Neter <pierreneter@gmail.com>
2017-12-01 02:54:16 +0700

    Fix get selected nav

--- scripts/apply-active-class-to-active-links.js
+++ scripts/apply-active-class-to-active-links.js
@@ -1,10 +1,13 @@
 module.exports = function applyActiveClassToActiveLinks () {
-  const topPath = location.pathname.split('/')[1]
+  const topPath = `/${location.pathname.split('/')[1]}`
 
   // homepage
   if (!topPath.length) return
 
-  document.querySelectorAll(`nav a[href^="/${topPath}"]`).forEach(a => {
-    a.classList.add('active')
+  document.querySelectorAll(`nav a`).forEach(a => {
+    let regexCheck = new RegExp(`^${a.getAttribute('href')}/?.*?`)
+    if (regexCheck.test(topPath)) {
+      a.classList.add('active')
+    }
   })
 }
