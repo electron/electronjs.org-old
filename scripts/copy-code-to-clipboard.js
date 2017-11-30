510ea31cce741bd05dacdc94185f6384d169e4ea
Zeke Sikelianos <zeke@sikelianos.com>
2017-11-30 11:35:33 -0800

    refactor clipboard copy code

--- scripts/copy-code-to-clipboard.js
+++ scripts/copy-code-to-clipboard.js
@@ -1,26 +1,26 @@
 const Clipboard = require('clipboard')
+const clipboard = new Clipboard('.btn-clipboard')
 
-let clipboard = new Clipboard('.btn-clipboard') // eslint-disable-line
-
-clipboard.on('success', function(e) { e.clearSelection() })
+clipboard.on('success', (e) => { 
+  e.clearSelection()
+})
 
 module.exports = function copyCodeToClipBoard () {
   document.querySelectorAll(`code.hljs`).forEach(code => {
-    let id = `_${Math.random().toString(36).substr(3)}`
+    const id = `_${Math.random().toString(36).substr(5)}`
     code.id = id
-    let copyButton = document.createElement('button')
-    copyButton.classList.add('btn-clipboard', 'simptip-position-top')
-    copyButton.setAttribute('data-tooltip', 'Copy to clipboard')
-    copyButton.setAttribute('data-clipboard-target', `#${id}`)
-    copyButton.innerHTML = 'Copy'
-    code.parentElement.appendChild(copyButton)
+    const button = document.createElement('button')
+    button.classList.add('btn-clipboard', 'simptip-position-top')
+    button.setAttribute('data-tooltip', 'Copy to clipboard')
+    button.setAttribute('data-clipboard-target', `#${id}`)
+    button.innerHTML = 'Copy'
+    code.parentElement.appendChild(button)
   })
+
   document.querySelectorAll('.btn-clipboard').forEach(button => {
     button.addEventListener('click', function (e) {
       e.target.setAttribute('data-tooltip', 'Copied')
     })
-  })
-  document.querySelectorAll('.btn-clipboard').forEach(button => {
     button.addEventListener('mouseout', function (e) {
       e.target.setAttribute('data-tooltip', 'Copy to clipboard')
       e.target.blur()
