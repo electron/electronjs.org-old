ff176a8a037b07af4a862ae6ffde1b8ce6609623
Zeke Sikelianos <zeke@sikelianos.com>
2017-12-15 21:33:12 -0800

    move doc history to its own route

--- routes/docs/show.js
+++ routes/docs/show.js
@@ -1,6 +1,4 @@
 const i18n = require('../../lib/i18n')
-const semver = require('semver')
-const historian = require('electron-api-historian')
 
 module.exports = (req, res, next) => {
   const doc = i18n.docs[req.language][req.path]
@@ -15,20 +13,6 @@ module.exports = (req, res, next) => {
     },
     layout: 'docs'
   })
-
-  // Renders a version history for the given doc
-  // e.g. /docs/api/app?history
-  if ('history' in req.query) {
-    // find releases that include the current doc
-    const filenameKey = context.doc.href.replace(/^\//, '') + '.md'
-    context.doc.birthTag = historian[filenameKey]
-    context.releasesIncludingCurrentDoc = context.releases.filter(release => {
-      return semver.gte(release.version, context.doc.birthTag.replace('v', ''))
-    })
-    context.viewingDocHistory = true
-    context.page.title = `${doc.title} Version History | Electron`
-    return res.render('docs/history', context)
-  }
 
   res.render('docs/show', context)
 }
