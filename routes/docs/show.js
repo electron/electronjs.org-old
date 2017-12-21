1a227cf90f0ffc2a4b37da541aff9536fe50ccdd
Pierre Neter <pierreneter@gmail.com>
2017-12-22 03:33:17 +0700

    Translate doc url

--- routes/docs/show.js
+++ routes/docs/show.js
@@ -14,5 +14,9 @@ module.exports = (req, res, next) => {
     layout: 'docs'
   })
 
+  if (!doc.translateUrl) {
+    doc.translateUrl = 'https://crowdin.com/project/electron'    
+  }
+  
   res.render('docs/show', context)
 }
