7758d90d49b049029ac9d3b153d2447888911fbd
Zeke Sikelianos <zeke@sikelianos.com>
2017-12-22 16:54:23 -0800

    do not assert specfic electron version in test

--- test/index.js
+++ test/index.js
@@ -121,7 +121,7 @@ describe('electronjs.org', () => {
         .trim()
         .replace(/\n/g, '')
         .replace(/\s+/g, ' ')
-        .should.equal('Docs / API / BrowserWindow v1.7.9')
+        .should.match('Docs / API / BrowserWindow v')
     })
 
     test('redirects pre-1.0 docs URLs', async () => {
