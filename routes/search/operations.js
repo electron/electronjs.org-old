2b99d7bdcf84cd65b27f4eac5b12cf7eaa91a35d
Zeke Sikelianos <zeke@sikelianos.com>
2018-01-23 20:16:35 -0800

    less is more

--- routes/search/operations.js
+++ routes/search/operations.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     name: 'docs',
-    displayName: 'Documentation',
+    displayName: 'Docs',
     octicon: 'file-text',
     query: `query FilterDocs($filter: String) {
       docs(filter: $filter){
