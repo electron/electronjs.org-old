834a7c948e1dc186ddea8b86e8b1f0aa1491bff2
Pierre Neter <pierreneter@gmail.com>
2018-02-03 14:49:38 +0700

    Move scripts to footer and define scripts for app

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+  require('./lazy-load-images')()
   require('./get-localized-strings')()
   require('./create-filter-list')()
   require('./fix-platform-labels')()
-  require('./update-demo-app-download-link')()
-  require('./update-app-download-links')()
+  if (window.app) {
+    require('./update-demo-app-download-link')()
+    require('./update-app-download-links')()
+  }
   require('./platform-specific-content')()
   require('./apply-active-class-to-active-links')()
   require('./remove-scheme-from-link-text')()
-  require('./lazy-load-images')()
   require('browser-date-formatter')()
   require('./copy-code-to-clipboard')()
 })
