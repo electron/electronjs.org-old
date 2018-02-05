b55b683a300279dcd4ef8601eb7d97583ceb84c5
Vanessa Yuen <vanessa.yh.yuen@gmail.com>
2018-02-05 17:29:59 +0100

    make use of new property `sections` instead of `html`

--- scripts/docs-language-toggle.js
+++ scripts/docs-language-toggle.js
@@ -1,4 +1,4 @@
-module.exports = function() {
+module.exports = () => {
   // don't add any listener if there isn't at least 1 non-english section
   if (document.querySelectorAll('.docs div.sub-section:not([data-lang="en-US"])').length > 0) {
     const toggles = Array.from(document.querySelectorAll('.docs .en-toggle'))
@@ -9,7 +9,7 @@ module.exports = function() {
           if (el.matches('div.sub-section')) break
           el = el.parentElement;
         }
-        const otherEl = document.querySelector(`[data-name=${el.dataset.name}].hidden`)
+        const otherEl = document.querySelector(`[data-name="${el.dataset.name}"].hidden`)
         if (otherEl) {
           otherEl.classList.remove("hidden")
           el.classList.add('hidden')
