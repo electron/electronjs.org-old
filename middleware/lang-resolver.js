7e78b57afc41ced324ccedea743fe660963266e7
Juande Manjon <juande.manjon@advent.com>
2018-02-16 15:55:38 -0800

    clang-format

--- middleware/lang-resolver.js
+++ middleware/lang-resolver.js
@@ -9,45 +9,48 @@ var lowerLang = {}
 // Keep all the language/region and language in a object
 // with property names in lowercases to resolve the lang code
 locales.forEach(locale => {
-    const s = locale.split('-')
-    const lang = s.shift().toLowerCase()
-    const region = s.join(' ').toUpperCase()
+  const s = locale.split('-')
+  const lang = s.shift().toLowerCase()
+  const region = s.join(' ').toUpperCase()
 
-    lowerLangRegion[`${lang}-${region}`] = locale
+  lowerLangRegion[`${lang}-${region}`] = locale
 
-    // The first lang for each locale is the default one
-    // to resolve when the region is missing
-    if (!lowerLang.hasOwnProperty(lang)) {
-        lowerLang[lang] = locale;
-    }
+  // The first lang for each locale is the default one
+  // to resolve when the region is missing
+  if (!lowerLang.hasOwnProperty(lang)) {
+    lowerLang[lang] = locale
+  }
 })
 
 function resolve(lang) {
-    var lower = lang.toLowerCase()
-    if (lowerLangRegion.hasOwnProperty(lower))
-        return lowerLangRegion[lower]
+  var lower = lang.toLowerCase()
+  if (lowerLangRegion.hasOwnProperty(lower)) {
+    return lowerLangRegion[lower]
+  }
 
-    lower = lang.split('-').shift().toLowerCase();
-    if (lowerLang.hasOwnProperty(lower))
-        return lowerLang[lower]
+  lower = lang.split('-').shift().toLowerCase()
+  if (lowerLang.hasOwnProperty(lower)) {
+    return lowerLang[lower]
+  }
 
-    return lang;
+  return lang
 }
 
 module.exports = function langResolver(req, res, next) {
-    if (req.query.lang) {
-        // resolving lang to lang-REGION when
-        //    - cases does not match
-        //    - region is missing
-        const rlang = resolve(req.query.lang)
+  if (req.query.lang) {
+    // resolving lang to lang-REGION when
+    //    - cases does not match
+    //    - region is missing
+    const rlang = resolve(req.query.lang)
 
-        // redirect only when the resolved lang exists and it is different
-        if (rlang && rlang !== req.query.lang) {
-            const parsedUrl = parseUrl(req);
-            req.query.lang = rlang;
-            return res.redirect(url.format({ pathname: parsedUrl.pathname, query: req.query }));
-        }
+    // redirect only when the resolved lang exists and it is different
+    if (rlang && rlang !== req.query.lang) {
+      const parsedUrl = parseUrl(req)
+      req.query.lang = rlang
+      return res.redirect(
+          url.format({pathname: parsedUrl.pathname, query: req.query}))
     }
+  }
 
-    return next();
+  return next()
 }
