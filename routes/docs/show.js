543a740279c10709d38ae988c800654b83714d7b
Zeke Sikelianos <zeke@sikelianos.com>
2018-03-01 12:51:39 -0800

    generate crowdin URL

--- routes/docs/show.js
+++ routes/docs/show.js
@@ -6,8 +6,12 @@ module.exports = (req, res, next) => {
   const docEn = req.context.currentLocale.startsWith('en')
                 ? null : i18n.docs['en-US'][req.path]
   if (!doc) return next()
-
+  
+  // Crowdin's undocumented mystery locale URL format. See https://git.io/vADu0
+  // e.g. `zh-CN` -> `zhcn`
   const crowdinLanguageCode = getProp(i18n, `locales.${req.context.currentLocale}.stats.code`)
+    .toLowerCase().replace('-', '') 
+
   doc.crowdinUrl = crowdinLanguageCode
     ? `https://crowdin.com/translate/electron/${doc.crowdinFileId}/en-${crowdinLanguageCode}`
     : 'https://crowdin.com/project/electron'
