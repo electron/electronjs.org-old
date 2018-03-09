5459b31a398ef9fb1e467b7c416f566d5383a7f9
Vanessa Yuen <vanessa.yh.yuen@gmail.com>
2018-03-09 18:38:40 +0100

    make script nicer and add comments

--- script/update-deps.sh
+++ script/update-deps.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env bash
 
+# This script checks for then apply updates from a list of
+# trusted dependencies.
+
 set -x            # print commands before execution
 set -o errexit    # always exit on error
 set -o pipefail   # honor exit codes when piping
 set -o nounset    # fail on unset variables
 
+TRUSTED_DEPENDENCIES="electron-i18n electron-apps electron-api-historian electron-releases"
+
 git clone "https://github.com/electron/electronjs.org" website
 cd website
 npm install
-npm update electron-i18n electron-apps electron-api-historian electron-releases
+npm update $TRUSTED_DEPENDENCIES
 
 if [ "$(git status --porcelain)" = "" ]; then
   echo "no new dependency to update"
   exit
-else
-  echo "updated some dependencies"
 fi
 
-# `pretest` script will run the build first
+echo "updated some dependencies."
 npm test
 
-# git config user.email electron@github.com
-# git config user.name electron-bot
-# git add .
-# git commit -am "chore: update electron module dependencies"
-# git push origin master --follow-tags
+git config user.email electron@github.com
+git config user.name electron-bot
+git add package.json package-lock.json
+git commit -am "chore: update dependencies"
+git push origin master --follow-tags
