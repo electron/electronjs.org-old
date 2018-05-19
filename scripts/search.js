fd843cbab39de7ed5d4732eb5008f8173a937d99
Emily Jordan <emily.h.jordan@gmail.com>
2018-05-19 17:44:25 +0200

    clean up

--- scripts/search.js
+++ scripts/search.js
@@ -6,15 +6,13 @@
 // const apps = require("../lib/apps");
 // const packages = require("../lib/packages");
 
-
 // const results = chain([apis, tutorials, apps, packages])
 //   .flatten()
 //   .value();
 
 // process.stdout.write(JSON.stringify(results, null, 2));
 
-const instantsearch = require("instantsearch.js");
-
+const instantsearch = require('instantsearch.js')
 
 const hitTemplate = `
 {{#_highlightResult.icon64}}
@@ -28,60 +26,58 @@ const hitTemplate = `
   <b>{{{_highlightResult.title.value}}}</b> -
   {{{_highlightResult.tldr.value}}}
 {{/_highlightResult.icon64}}
-`;
+`
 
 module.exports = () => {
-
   const search = instantsearch({
-    appId: "L9LD9GHGQJ",
-    apiKey: "24e7e99910a15eb5d9d93531e5682370",
-    indexName: "electron-apis",
+    appId: 'L9LD9GHGQJ',
+    apiKey: '24e7e99910a15eb5d9d93531e5682370',
+    indexName: 'electron-apis',
     routing: true
-  });
+  })
 
   search.addWidget(
     instantsearch.widgets.hits({
-      container: "#hits",
+      container: '#hits',
       templates: {
-        empty: "No results",
+        empty: 'No results',
         item: hitTemplate
       },
       transformData: {
         item: data => {
           // useful for viewing template context:
-          console.log("data", data);
-          return data;
+          console.log('data', data)
+          return data
         }
       }
     })
-  );
+  )
 
   search.addWidget(
     instantsearch.widgets.searchBox({
-      container: "#search-box",
-      placeholder: "Search Electron APIs"
+      container: '#search-box',
+      placeholder: 'Search Electron APIs'
     })
-  );
+  )
 
   search.addWidget(
     instantsearch.widgets.refinementList({
-      container: "#refinement-list",
-      attributeName: "type",
+      container: '#refinement-list',
+      attributeName: 'type',
       limit: 10,
       templates: {
-        header: "Types"
+        header: 'Types'
       }
     })
-  );
+  )
 
-  search.start();
+  search.start()
 
-  search.on("render", (...args) => {
+  search.on('render', (...args) => {
     // console.log('algolia render', args)
-  });
+  })
 
-  search.on("error", (...args) => {
-    console.log("algolia error", args);
-  });
+  search.on('error', (...args) => {
+    console.log('algolia error', args)
+  })
 }
-
