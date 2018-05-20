de020d9ce2ac1312c19d0b17ab464fe3b49027e9
Emily Jordan <emily.h.jordan@gmail.com>
2018-05-20 16:06:32 +0200

    removed json build from search script

--- scripts/search.js
+++ scripts/search.js
@@ -1,17 +1,3 @@
-#!/usr/bin/env node
-
-// const { chain } = require("lodash");
-// const apis = require("../lib/electron-apis");
-// const tutorials = require("../lib/tutorials");
-// const apps = require("../lib/apps");
-// const packages = require("../lib/packages");
-
-// const results = chain([apis, tutorials, apps, packages])
-//   .flatten()
-//   .value();
-
-// process.stdout.write(JSON.stringify(results, null, 2));
-
 const instantsearch = require('instantsearch.js')
 
 const hitTemplate = `
