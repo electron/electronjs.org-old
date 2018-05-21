12b8842bc7ec87d72626949e1dd5ebf3ffd8d191
Emily Jordan <emily.h.jordan@gmail.com>
2018-05-21 17:53:22 +0200

    heavier in search styling

--- scripts/search.js
+++ scripts/search.js
@@ -1,24 +1,36 @@
 const instantsearch = require('instantsearch.js')
 
-// let searchInput = document.getElementById("search-input");
-// searchInput.onfocus = () => {
+let searchInput = document.getElementById("search-input");
+searchInput.onclick = () => {
+  searchInput.classList.add("active")
+}
 
-// }
+searchInput.onmouseout = () =>{
+  searchInput.classList.remove("active")
+}
 
 const hitTemplate = `
 {{#_highlightResult.icon64}}
   <div class="app-hit-container">
     <img src="https://electronjs.org/node_modules/electron-apps/apps/{{_highlightResult.slug.value}}/{{_highlightResult.icon64.value}}">
-    <b>{{{_highlightResult.name.value}}}</b> -
-    {{{_highlightResult.description.value}}}
+    <div>
+      <div class="hit-type" >{{{type}}}</div>
+      <b style="font-size:14pt" >{{{_highlightResult.name.value}}}</b>
+      <div style="font-size:11pt" >{{{_highlightResult.description.value}}}</div>
+      <a href={{{website}}} ><span class="octicon octicon-link-external" /></a>
+      <a href={{{repository}}} ><span class="octicon octicon-mark-github" /></a>
+    </div>
   </div>
 {{/_highlightResult.icon64}}
 
 {{^_highlightResult.icon64}}
   <div className="other-hit-container">
-    {{{type.value}}}
-    <b>{{{_highlightResult.title.value}}}</b> -
-    {{{_highlightResult.tldr.value}}}
+    <div class="hit-type" >{{{type}}}</div>
+    <div>
+      <b><a href={{{url}}} style="text-decoration: none" >{{{_highlightResult.title.value}}}</a></b>
+      <div style="font-size: 10pt">{{{_highlightResult.tldr.value}}}</div>
+    </div>
+    <a href={{{url}}}>More <span class="octicon octicon-chevron-right" /></a>
   </div>
 {{/_highlightResult.icon64}}
 `;
