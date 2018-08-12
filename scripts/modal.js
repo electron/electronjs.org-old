ee3ff64c04d914252973d23ae79c1dc84cb492c9
Ashley Zeldin <ashley.zeldin@gmail.com>
2018-08-11 21:52:52 -0700

    First pass at implementing modal dialog for keyboard cheatsheet
    + Created scripts/modal.js, styles/_modal.scss, views/partials/modal.html
    + Updated scripts/index.js, styles/index.scss, views/layouts/main.html to include modal files
    
    +Bonus: corrected minor typo in bower_components/basecoat.readme.md

---
+++ scripts/modal.js
@@ -0,0 +1,30 @@
+module.exports = () => {
+	// Get the modal
+	var modal = document.getElementById('myModal');
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user types ?, open the modal 
+	window.addEventListener("keydown", function(event) {
+		if (event.key == "?") {
+	    if (event.target == null || event.target.tagName != "INPUT") {
+				modal.style.display = "block";
+				event.stopImmediatePropagation();
+				event.preventDefault();
+			}
+		}
+	}, true);
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	    modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	    }
+	}
+}
