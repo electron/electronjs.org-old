cf1e4fee1d557a0e2d87369bff7712e73a5f34a5
HashimotoYT <hashimoto.stream@gmail.com>
2018-12-11 23:48:16 +0200

    Replace Simptip with Primer

--- scripts/copy-code-to-clipboard.js
+++ scripts/copy-code-to-clipboard.js
@@ -10,8 +10,8 @@ module.exports = function copyCodeToClipBoard () {
     const id = `_${Math.random().toString(36).substr(5)}`
     code.id = id
     const button = document.createElement('button')
-    button.classList.add('btn-clipboard', 'simptip-position-top')
-    button.setAttribute('data-tooltip', window.localized.clipboard.copy_to_clipboard)
+    button.classList.add('btn-clipboard', 'tooltipped', 'tooltipped-n', 'border', 'float-left')
+    button.setAttribute('aria-label', window.localized.clipboard.copy_to_clipboard)
     button.setAttribute('data-clipboard-target', `#${id}`)
     button.innerHTML = window.localized.clipboard.copy
     code.parentElement.appendChild(button)
@@ -19,11 +19,11 @@ module.exports = function copyCodeToClipBoard () {
 
   document.querySelectorAll('.btn-clipboard').forEach(button => {
     button.addEventListener('mouseout', function (e) {
-      e.target.setAttribute('data-tooltip', window.localized.clipboard.copy_to_clipboard)
+      e.target.setAttribute('aria-label', window.localized.clipboard.copy_to_clipboard)
       e.target.blur()
     })
     button.addEventListener('click', function (e) {
-      e.target.setAttribute('data-tooltip', window.localized.clipboard.copied)
+      e.target.setAttribute('aria-label', window.localized.clipboard.copied)
     })
   })
 }
