c9008c2d10400999ffedc2e626b370568e4b0b49
Michelle Tilley <michelle@michelletilley.net>
2018-12-19 13:45:15 -0800

    remove unused filter

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -2,11 +2,7 @@ self.addEventListener('activate', function (event) {
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.filter(function (cacheName) {
-          // Return true if you want to remove this cache,
-          // but remember that caches are shared across
-          // the whole origin
-        }).map(function (cacheName) {
+        cacheNames.map(function (cacheName) {
           return caches.delete(cacheName)
         })
       )
