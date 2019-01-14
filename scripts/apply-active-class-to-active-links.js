bf547e250448f052d3614351ba582f9793d3a343
Michelle Tilley <michelle@michelletilley.net>
2019-01-14 10:59:08 -0800

    Ensure link hrefs are escaped in regex

--- scripts/apply-active-class-to-active-links.js
+++ scripts/apply-active-class-to-active-links.js
@@ -1,3 +1,5 @@
+const { escapeRegExp } = require('lodash')
+
 module.exports = function applyActiveClassToActiveLinks () {
   const topPath = `/${location.pathname.split('/')[1]}`
 
@@ -5,7 +7,8 @@ module.exports = function applyActiveClassToActiveLinks () {
   if (!topPath.length) return
 
   document.querySelectorAll(`nav a`).forEach(a => {
-    let regexCheck = new RegExp(`^${a.getAttribute('href')}/?.*?`)
+    const href = escapeRegExp(a.getAttribute('href'))
+    let regexCheck = new RegExp(`^${href}/?.*?`)
     if (regexCheck.test(topPath)) {
       a.classList.add('active')
     }
