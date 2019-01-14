4ee22d9279448cec6620bf23355c4cbdae4aa3b8
Michelle Tilley <michelle@michelletilley.net>
2019-01-14 11:05:20 -0800

    Exclude nightly releases from version history

--- routes/docs/history.js
+++ routes/docs/history.js
@@ -2,6 +2,8 @@ const i18n = require('../../lib/i18n')
 const semver = require('semver')
 const historian = require('electron-api-historian')
 
+const isNightly = version => semver.parse(version).prerelease.includes('nightly')
+
 module.exports = (req, res, next) => {
   const doc = i18n.docs[req.language][req.path.replace('/history', '')]
   if (!doc) return next()
@@ -12,7 +14,7 @@ module.exports = (req, res, next) => {
 
   if (doc.birthTag) {
     doc.releases = req.context.releases.filter(release => {
-      return semver.gte(release.version, doc.birthTag.replace('v', ''))
+      return semver.gte(release.version, doc.birthTag.replace('v', '')) && !isNightly(release.version)
     })
   } else {
     doc.releases = []
