acfd4a6cce6bf8eefde45000b593e212d29249ea
HashimotoYT <hashimoto.stream@gmail.com>
2019-01-31 21:03:48 +0200

    Add debug build with preview icons

--- script/generate-octicons.js
+++ script/generate-octicons.js
@@ -5,7 +5,9 @@ const fs = require('fs-extra')
 
 generate()
 
-async function generate () {
+async function generate (debug) {
+  debug = process.argv[2]
+
   console.log('Copy octicons to temporary directory…')
   await fs.copy('./node_modules/octicons/build/svg', './script/generate-octicons/_temp').catch(err => console.log(err))
 
@@ -13,8 +15,13 @@ async function generate () {
   await fs.remove('./styles/octicons')
   await fs.mkdirp('./styles/octicons')
 
-  console.log('Generating octicons font stylesheet…')
-  await cp.execSync("./node_modules/.bin/icon-font-generator ./script/generate-octicons/_temp/*.svg -o ./styles/octicons -n octicons -p octicon --csstp ./script/generate-octicons/template/style.hbs --height 1000 --center --codepoints ./script/generate-octicons/template/mapping.json --descent 180 --json false --types 'svg, ttf' --html false")
+  if (debug) {
+    console.log('Generating debug octicons font stylesheet with preview file…')
+    await cp.execSync("./node_modules/.bin/icon-font-generator ./script/generate-octicons/_temp/*.svg -o ./styles/octicons -n octicons -p octicon --csstp ./script/generate-octicons/template/style.hbs --height 1000 --center --codepoints ./script/generate-octicons/template/mapping.json --descent 180 --json false --types 'svg, ttf' --html true --htmltp ./script/generate-octicons/template/preview.hbs")
+  } else {
+    console.log('Generating octicons font stylesheet…')
+    await cp.execSync("./node_modules/.bin/icon-font-generator ./script/generate-octicons/_temp/*.svg -o ./styles/octicons -n octicons -p octicon --csstp ./script/generate-octicons/template/style.hbs --height 1000 --center --codepoints ./script/generate-octicons/template/mapping.json --descent 180 --json false --types 'svg, ttf' --html false")
+  }
 
   console.log('Hide and Seek or Remove _temp folder…')
   await fs.remove('./script/generate-octicons/_temp')
