0d44d49d13036ea5153035fbd2a3fdd1239b1589
HashimotoYT <hashimoto.stream@gmail.com>
2019-01-31 19:08:27 +0200

    Clean octicons folder before generating

--- script/generate-octicons.js
+++ script/generate-octicons.js
@@ -9,6 +9,10 @@ async function generate () {
   console.log('Copy octicons to temporary directory…')
   await fs.copy('./node_modules/octicons/build/svg', './script/generate-octicons/_temp').catch(err => console.log(err))
 
+  console.log('Clean octicons directory')
+  await fs.remove('./styles/octicons')
+  await fs.mkdirp('./styles/octicons')
+
   console.log('Generating octicons font stylesheet…')
   await cp.execSync("./node_modules/.bin/icon-font-generator ./script/generate-octicons/_temp/*.svg -o ./styles/octicons -n octicons -p octicon --csstp ./script/generate-octicons/template/style.hbs --height 1000 --center --codepoints ./script/generate-octicons/template/mapping.json --descent 180 --json false --types 'svg, ttf' --html false")
 
