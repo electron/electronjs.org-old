16d0b2b7d589b268234b9f18fe2c52e05d42b942
HashimotoYT <hashimoto.stream@gmail.com>
2019-02-05 17:14:54 +0200

    Update paths

--- script/generate-octicons.js
+++ script/generate-octicons.js
@@ -6,21 +6,21 @@ const fs = require('fs-extra')
 generate()
 
 async function generate (debug) {
-  debug = process.argv[2]
+  debug = process.argv.indexOf('--debug') < -1
 
   console.log('Copy octicons to temporary directory…')
   await fs.copy('./node_modules/octicons/build/svg', './script/generate-octicons/_temp').catch(err => console.log(err))
 
   console.log('Clean octicons directory')
-  await fs.remove('./styles/octicons')
-  await fs.mkdirp('./styles/octicons')
+  await fs.remove('./public/styles/octicons')
+  await fs.mkdirp('./public/styles/octicons')
 
   if (debug) {
     console.log('Generating debug octicons font stylesheet with preview file…')
-    await cp.execSync("./node_modules/.bin/icon-font-generator ./script/generate-octicons/_temp/*.svg -o ./styles/octicons -n octicons -p octicon --csstp ./script/generate-octicons/template/style.hbs --height 1000 --center --codepoints ./script/generate-octicons/template/mapping.json --descent 180 --json false --types 'svg, ttf' --html true --htmltp ./script/generate-octicons/template/preview.hbs")
+    await cp.execSync("./node_modules/.bin/icon-font-generator ./script/generate-octicons/_temp/*.svg -o ./public/styles/octicons -n octicons -p octicon --csstp ./script/generate-octicons/template/style.hbs --height 1000 --center --codepoints ./script/generate-octicons/template/mapping.json --descent 180 --json false --types 'svg, ttf' --html true --htmltp ./script/generate-octicons/template/preview.hbs")
   } else {
     console.log('Generating octicons font stylesheet…')
-    await cp.execSync("./node_modules/.bin/icon-font-generator ./script/generate-octicons/_temp/*.svg -o ./styles/octicons -n octicons -p octicon --csstp ./script/generate-octicons/template/style.hbs --height 1000 --center --codepoints ./script/generate-octicons/template/mapping.json --descent 180 --json false --types 'svg, ttf' --html false")
+    await cp.execSync("./node_modules/.bin/icon-font-generator ./script/generate-octicons/_temp/*.svg -o ./public/styles/octicons -n octicons -p octicon --csstp ./script/generate-octicons/template/style.hbs --height 1000 --center --codepoints ./script/generate-octicons/template/mapping.json --descent 180 --json false --types 'svg, ttf' --html false")
   }
 
   console.log('Hide and Seek or Remove _temp folder…')
