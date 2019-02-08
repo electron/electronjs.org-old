677b4c73b90d75f1bd131021e3405c9942f3a102
HashimotoYT <hashimoto.stream@gmail.com>
2019-02-08 23:49:30 +0200

    💄

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,34 +1,34 @@
-module.exports = function installModal() {
+module.exports = function installModal () {
   // Get the modal
-  const modal = document.getElementById('myModal');
+  const modal = document.getElementById('myModal')
 
   // Get the <span> element that closes the modal
-  const span = document.getElementsByClassName("close")[0];
+  const span = document.getElementsByClassName('close')[0]
 
   // When the user types ?, open the modal
-  window.addEventListener("keydown", event => {
-    if (event.key === "?") {
-      if (event.target === null || event.target.tagName !== "INPUT") {
-        modal.style.display = "block";
-        event.stopImmediatePropagation();
-        event.preventDefault();
+  window.addEventListener('keydown', event => {
+    if (event.key === '?') {
+      if (event.target === null || event.target.tagName !== 'INPUT') {
+        modal.style.display = 'block'
+        event.stopImmediatePropagation()
+        event.preventDefault()
       }
     }
 
     if (event.keyCode === 27) {
-      modal.style.display = "none";
+      modal.style.display = 'none'
     }
-  }, true);
+  }, true)
 
   // When the user clicks on <span> (x), close the modal
   span.onclick = () => {
-    modal.style.display = "none";
+    modal.style.display = 'none'
   }
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = (event) => {
     if (event.target === modal) {
-      modal.style.display = "none";
+      modal.style.display = 'none'
     }
   }
 }
