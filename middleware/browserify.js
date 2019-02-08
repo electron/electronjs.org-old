733493b261833ef9ae8ef79d9a5ed5a9c6b9663d
Michelle Tilley <michelle@michelletilley.net>
2019-02-08 14:53:37 -0800

    Simplify browserify middleware

--- middleware/browserify.js
+++ middleware/browserify.js
@@ -1,8 +1,4 @@
 const browserify = require('browserify-middleware')
 const browserifyOptions = require('./browserify-opts')
 
-function babelifyMiddleware (entry) {
-  return browserifyOptions(browserify)(entry)
-}
-
-module.exports = babelifyMiddleware
+module.exports = browserifyOptions(browserify)
