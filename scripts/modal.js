5861c43df34e0e87011bfbef20d54d921a302db3
HashimotoYT <hashimoto.stream@gmail.com>
2019-02-12 16:26:30 +0200

    ⬆ z-index to 5

--- scripts/modal.js
+++ scripts/modal.js
@@ -20,7 +20,6 @@ module.exports = function installModal () {
     }
   }, true)
 
-  // When the user clicks on <span> (x), close the modal
   // When the user clicks anywhere outside of the modal, close it
   window.addEventListener('click', (ev) => {
     if (ev.target === modal) {
