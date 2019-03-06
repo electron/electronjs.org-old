09b7fd0b989fb8ec69c553d9f3b91c3cdfceefb3
HashimotoYT <hashimoto.stream@gmail.com>
2019-03-06 18:44:10 +0200

    lint win me

--- script/verify-primer.js
+++ script/verify-primer.js
@@ -6,33 +6,31 @@ verifyPrimer()
 // TODO: Replace by find-me-value module
 function findMeValue (text, key) {
   if (typeof text !== 'string' || typeof key !== 'string') {
-    throw new TypeError('Expected a string');
+    throw new TypeError('Expected a string')
   }
 
-  const regex = new RegExp(`${key}.(.*)$`, 'm');
-  const match = regex.exec(text);
+  const regex = new RegExp(`${key}.(.*)$`, 'm')
+  const match = regex.exec(text)
   if (match) {
-    return match[1];
+    return match[1]
   } else {
     return null
   }
 }
 
-async function verifyPrimer() {
+async function verifyPrimer () {
   const url = `https://cdn.jsdelivr.net/gh/primer/primer@master/package.json`
   const localVersion = fs.readFileSync('./public/styles/primer/.version', 'utf8').slice(0, 6) // ugh hacks
 
   fetch(url)
-  .then(res => res.text())
-  .then(body => {
-    // hacks 🤖
-    typeof body === 'string'
-    const masterVersion = findMeValue(body, '"version":').replace(/\",/, '').replace(/\"/, '') // simplfiy
+    .then(res => res.text())
+    .then(body => {
+      const masterVersion = findMeValue(body, '"version":').replace(/",/, '').replace(/"/, '') // simplfiy
 
-    if (masterVersion !== localVersion) {
-      console.log(`WARNING: Local version of Primer behind version on npm:\n - Local Version: ${localVersion}\n - Version on npm: ${masterVersion}`)
-    } else if (masterVersion === localVersion) {
-      console.log('All fine')
-    }
-  })
+      if (masterVersion !== localVersion) {
+        console.log(`WARNING: Local version of Primer behind version on npm:\n - Local Version: ${localVersion}\n - Version on npm: ${masterVersion}`)
+      } else if (masterVersion === localVersion) {
+        console.log('All fine')
+      }
+    })
 }
