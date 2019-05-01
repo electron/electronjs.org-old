32ae952a2e74b16debc39483f96f323fc230a476
Michelle Tilley <michelle@michelletilley.net>
2019-05-01 11:28:40 -0700

    Update random apps to 25 to match new styles

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,7 @@ const apps = require('../lib/apps')
 module.exports = (req, res) => {
   const randomizedApps = shuffle(apps.slice())
   const context = Object.assign(req.context, {
-    apps: randomizedApps.slice(0, 24)
+    apps: randomizedApps.slice(0, 25)
   })
 
   res.render('home', context)
