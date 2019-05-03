307615f5525b295a65091f89f0081bb4682c25a0
HashimotoYT <hashimoto.stream@gmail.com>
2019-05-03 20:31:42 +0300

    style: prettier

--- routes/headers.js
+++ routes/headers.js
@@ -1,10 +1,8 @@
-const fs = require('fs-extra');
-
 const DIST_URL = 'https://gh-contractor-zcbenz.s3.amazonaws.com/atom-shell/dist'
 
 module.exports = (req, res, next) => {
   console.log(req.params[0])
   const url = req.params[0]
   if (url === []) return next()
-  return res.redirect(`${DIST_URL}/${url}`);
+  return res.redirect(`${DIST_URL}/${url}`)
 }
--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use(requestLanguage({
   languages: Object.keys(i18n.locales),
   cookie: {
     name: 'language',
-    options: {maxAge: 30 * 24 * 60 * 60 * 1000},
+    options: { maxAge: 30 * 24 * 60 * 60 * 1000 },
     url: '/languages/{language}'
   }
 }))
