f853a8a81dcdb7288fa3db92a10cc8137409174a
HashimotoYT <hashimoto.stream@gmail.com>
2019-05-03 20:38:09 +0300

    test: add tests

--- routes/headers.js
+++ routes/headers.js
@@ -1,7 +1,6 @@
 const DIST_URL = 'https://gh-contractor-zcbenz.s3.amazonaws.com/atom-shell/dist'
 
 module.exports = (req, res, next) => {
-  console.log(req.params[0])
   const url = req.params[0]
   if (url === []) return next()
   return res.redirect(`${DIST_URL}/${url}`)
