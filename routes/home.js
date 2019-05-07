665b8331cff7e6373e615067abb9ea61e2e782eb
Michelle Tilley <michelle@michelletilley.net>
2019-05-07 09:34:42 -0700

    Update homepage featured company list:
    * Make each image the same size and type
    * Introduce module that exports company details
    * Generate company list programmatically

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,11 @@
 const shuffle = require('knuth-shuffle-seeded')
 const apps = require('../lib/apps')
+const featuredCompanies = require('../lib/featured-companies')
 
 module.exports = (req, res) => {
   const randomizedApps = shuffle(apps.slice())
   const context = Object.assign(req.context, {
+    companies: featuredCompanies,
     apps: randomizedApps.slice(0, 25)
   })
 
