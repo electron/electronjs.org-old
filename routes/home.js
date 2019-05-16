4e9792e8aff7c1f72c78ba2d2e701471e94cd4c0
Michelle Tilley <michelle@michelletilley.net>
2019-05-16 14:15:04 -0700

    feat: update homepage apps section (#2328)

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,12 @@
+const shuffle = require('knuth-shuffle-seeded')
 const apps = require('../lib/apps')
+const featuredCompanies = require('../lib/featured-companies')
 
 module.exports = (req, res) => {
+  const randomizedApps = shuffle(apps.slice())
   const context = Object.assign(req.context, {
-    apps: apps
+    companies: featuredCompanies,
+    apps: randomizedApps.slice(0, 25)
   })
 
   res.render('home', context)
