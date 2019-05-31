04877ada5bdebea2aa171166ba134f630f48339c
Felix Rieseberg <felix@felixrieseberg.com>
2019-05-31 16:49:09 +0200

    test: Fix test

--- test/index.js
+++ test/index.js
@@ -160,7 +160,7 @@ describe('electronjs.org', () => {
     test('index', async () => {
       const $ = await get('/docs')
       $('header').should.have.class('site-header')
-      $('a[href="/docs/tutorial/about"]').should.have.text('About Electron')
+      $('a[href="/docs/tutorial/development-environment#setting-up-macos"]').should.have.text('Setting up macOS')
       $('a[href="/docs/api/auto-updater"]').should.have.text('autoUpdater')
     })
 
