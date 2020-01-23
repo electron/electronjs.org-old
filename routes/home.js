d2cfda0e57d3196b46e01de32357b2cba2180bc8
HashimotoYT <hashimoto.stream@gmail.com>
2020-01-23 14:21:01 +0200

    announce -> announcement

--- routes/home.js
+++ routes/home.js
@@ -2,14 +2,14 @@ const shuffle = require('knuth-shuffle-seeded')
 const apps = require('../lib/apps')
 const featuredCompanies = require('../lib/featured-companies')
 
-const showAnnounceBanner = new Date() < new Date(2020, 1, 1)
+const showAnnouncementBanner = new Date() < new Date(2020, 1, 1)
 
 module.exports = (req, res) => {
   const randomizedApps = shuffle(apps.slice())
   const context = Object.assign(req.context, {
     companies: featuredCompanies,
     apps: randomizedApps.slice(0, 25),
-    showAnnounceBanner,
+    showAnnouncementBanner,
   })
 
   res.render('home', context)
