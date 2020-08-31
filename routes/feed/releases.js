f69303ffbaf7c714b3dc922c6522939d4b27f7c6
Samuel Attard <sattard@slack-corp.com>
2020-08-31 11:43:26 -0700

    feat: add support for channel filtering on the RSS feed (#4351)
    Co-authored-by: Erick Zhao <erick@hotmail.ca>
    Co-authored-by: Vlad Hashimoto <hashimoto.stream@gmail.com>

--- routes/feed/releases.js
+++ routes/feed/releases.js
@@ -1,7 +1,41 @@
+const memoize = require('fast-memoize')
+
 const { setupFeed } = require('./mainFeed')
 
+const VALID_TYPES = new Set(['stable', 'beta', 'nightly'])
+
+const getReleasesForTypes = memoize((all, queryTypes) => {
+  const releases = []
+  const types = queryTypes.split(',')
+  for (const type of types) {
+    if (!VALID_TYPES.has(type)) continue
+
+    releases.push(
+      ...all.filter((r) => {
+        switch (type) {
+          case 'stable':
+            return r.isStable()
+          case 'beta':
+            return r.isBeta()
+          case 'nightly':
+            return r.isNightly()
+        }
+        return false
+      })
+    )
+  }
+
+  return releases
+})
+
 module.exports = async function feedHandler(req, res, next) {
-  const feed = await setupFeed('releases', req.context.releases.all)
+  const { types } = req.query
+  let releases = req.context.releases.all
+  if (types) {
+    releases = getReleasesForTypes(releases, types)
+  }
+
+  const feed = await setupFeed('releases', releases)
 
   if (req.path === '/releases.xml') {
     res.set('content-type', 'text/xml')
