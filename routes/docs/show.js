4ad4e0b27a8e417b58c5cd8212a664c27b3d5769
Erick Zhao <erick@hotmail.ca>
2021-01-28 12:02:33 -0800

    wip: server-side docs TOC

--- routes/docs/show.js
+++ routes/docs/show.js
@@ -1,5 +1,6 @@
 const i18n = require('../../lib/i18n')
 const editorCodes = require('crowdin-editor-language-codes')
+const fs = require('fs')
 
 module.exports = (req, res, next) => {
   const doc = i18n.docs[req.language][req.path]
@@ -12,6 +13,8 @@ module.exports = (req, res, next) => {
   // e.g. `zh-CN` -> `zhcn`
   const { languageCode } = i18n.locales[req.context.currentLocale] || {}
   const { editorCode } = editorCodes[languageCode] || {}
+
+  generateTableOfContents(doc)
 
   doc.crowdinUrl = editorCode
     ? `https://crowdin.com/translate/electron/${doc.crowdinFileId}/en-${editorCode}`
@@ -29,4 +32,49 @@ module.exports = (req, res, next) => {
   })
 
   res.render('docs/show', context)
+}
+
+function generateTableOfContents(doc) {
+  const h1 = doc.sections[0]
+  const toc = {
+    name: h1.name,
+    slug: h1.slug,
+    level: h1.level,
+    children: [],
+  }
+
+  let parent = toc
+  let currLevel = 2
+
+  for (const section of doc.sections.slice(1, doc.sections.length - 1)) {
+    if (section.level === currLevel) {
+      parent.children.push({
+        name: section.name,
+        slug: section.slug,
+        level: section.level, // TODO: remove
+        children: [],
+      })
+    } else if (section.level > currLevel) {
+      parent = parent.children[parent.children.length - 1]
+      parent.children.push({
+        name: section.name,
+        slug: section.slug,
+        children: [],
+      })
+      currLevel++
+    } else {
+      parent = toc
+      for (let i = 1; i < section.level - 1; i++) {
+        parent = parent.children[parent.children.length - 1]
+      }
+      parent.children.push({
+        name: section.name,
+        slug: section.slug,
+        children: [],
+      })
+      currLevel = section.level
+    }
+  }
+  fs.writeFileSync('./hello.json', JSON.stringify(toc, null, 2))
+  console.log(JSON.stringify(toc, null, 2))
 }
