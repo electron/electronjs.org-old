9fb2d62e21a41512113c8f64737744f8b2ad4923
Antón Molleda <amolleda@gmail.com>
2021-02-17 10:07:09 -0800

    fix: show `Node.js` correctly in ToC
    One of the regex matched `Node.js` and returned `js` only when
    generating the ToC. E.g.:
    https://www.electronjs.org/docs/tutorial/quick-start

--- scripts/docs-table-of-contents.js
+++ scripts/docs-table-of-contents.js
@@ -15,21 +15,28 @@ function generateTableOfContents() {
       disableTocScrollSync: true, // don't scroll TOC with page scroll (looks bad with large lists)
       headingLabelCallback: (str) => {
         /**
-       * Clean up table of content strings using regex. We want to avoid
-       * having long and repetitive strings for our Events and APIs.
-
-       * 1. For events: `Event: 'close'` becomes `close`
-       * 2. For properties/methods: `win.previewFile(path[, displayName]) macOS`
-       *    becomes `previewFile`
-       */
-        let regexMatch
-        if ((regexMatch = str.match(/Event: '([a-z]*(?:-[a-z]+)*)'/))) {
-          return regexMatch[1]
-        } else if (
-          (regexMatch = str.match(/^[a-zA-Z]+\.((?:[a-zA-Z]+[\.]?)+)/))
-        ) {
-          return regexMatch[1]
+         * Clean up table of content strings using regex. We want to avoid
+         * having long and repetitive strings in the API section like the following:
+         *  * ipcMain.removeListener(channel, listener)
+         *  * ipcMain.removeAllListeners([channel])
+         *  * ipcMain.handle(channel, listener)
+         *  * ...
+         */
+        if (!window.location.pathname.startsWith('/docs/api/')) {
+          return str
         }
+
+        // For events: `Event: 'close'` becomes `close`
+        const eventsMatch = str.match(/Event: '([a-z]*(?:-[a-z]+)*)'/)
+        // For properties / methods: `win.previewFile(path[, displayName]) macOS` becomes `previewFile`
+        const propsMethodsMatch = str.match(/^[a-zA-Z]+\.((?:[a-zA-Z]+[\.]?)+)/)
+
+        if (eventsMatch) {
+          return eventsMatch[1]
+        } else if (propsMethodsMatch) {
+          return propsMethodsMatch[1]
+        }
+
         return str
       },
     })
